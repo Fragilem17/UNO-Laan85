6ed1c3af650e761b5062beb5c19432938ea3208b
Tom Goethals <fragilem17@gmail.com>
2018-09-02 22:53:40 +0200

    added steam integration and lots of small details

---
+++ Unity/Assets/Scripts/SetFocusDistance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SetFocusDistance : MonoBehaviour {
+
+	public LayerMask maskLayer = 1;
+	public Camera cam;
+	public Vector3 middle;
+
+	public PostProcessVolume profile;
+	private	DepthOfField _depthOfField;
+
+	// Use this for initialization
+	void Start () {
+
+		profile.profile.TryGetSettings<DepthOfField>(out _depthOfField);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = cam.ViewportPointToRay(middle, Camera.MonoOrStereoscopicEye.Mono);
+
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+		//print(maskLayer.value + " : " + gameObject.layer);
+		if (Physics.Raycast(ray, out hit, 10000, maskLayer))
+		{
+			float d = Vector3.Distance(transform.position, hit.point);
+			//print("RAYING " + hit.transform.gameObject.name + ": " + d);
+
+			_depthOfField.focusDistance.Override(d);
+
+		}
+	}
+}
